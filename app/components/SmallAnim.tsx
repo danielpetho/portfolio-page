b82eec277a5094976ddeccdc94aa68f8d0e43bdb
danielpetho <kpethodaniel@gmail.com>
2023-11-15 17:24:40 +0100

    setup colors, landing page under construction text

---
+++ app/components/SmallAnim.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export const Loading = () => (
+  <motion.div
+    className="w-64 h-64 bg-gray -z-10 absolute"
+    animate={{
+      scale: [1, 2, 2, 1, 1],
+      rotate: [0, 0, 180, 180, 0],
+      borderRadius: ["25%", "50%", "100%", "50%", "25%"],
+    }}
+    transition={{
+      duration: 3,
+      ease: "easeInOut",
+      times: [0, 0.2, 0.5, 0.8, 1],
+      repeat: Infinity,
+      repeatDelay: 1,
+    }}
+  />
+);
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Loading } from "./components/SmallAnim";
 
 export default function Home() {
   return (
-    <main>
-    
+    <main className="flex font-bold text-blue items-center justify-center flex-col h-screen text-center text-4xl sm:text-7xl mx-4">
+      <Loading />
+      <h2>This site is under construction.</h2>
+      <h3 className="my-2">Check back soon!</h3>
+      
     </main>
   )
 }
