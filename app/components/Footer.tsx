80094ddd94238ed52f263413e3fe122fadf6d8ab
emzeperix <kpethodaniel@gmail.com>
2023-12-07 11:08:12 +0100

    footer links with animation

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import Scene from "./R3F/Scene";
+import MaskedExternalLink from "./Text/MaskedExternalLink";
 
 
 const Footer: React.FC = () => {
@@ -13,7 +14,7 @@ const Footer: React.FC = () => {
           <div className="absolute top-0 left-0 z-10 h-full">
             <div className="flex flex-col p-24 h-full w-full flex-grow justify-between">
               <div>
-                <h2 className="text-9xl font-semibold ">
+                <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-semibold ">
                   Wanna create something cool together?
                 </h2>
               </div>
@@ -29,40 +30,12 @@ const Footer: React.FC = () => {
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row max-w-[1920px] px-12 sm:px-16 md:px-20 lg:px-32 bg-black pb-24 sm:pb-20 md:pb-16 lg:pb-12 pt-12 w-full justify-between">
-        <div className="flex flex-col xl:flex-row  lg:gap-x-24 md:gap-x-18  ">
-          <a
-            href="https://instagram.com/nonzeroexitcode"
-            className="cursor"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Instagram
-          </a>
-          <a
-            href="https://twitter.com/nonzeroexitcode"
-            className="cursor"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter / X
-          </a>
-          <a
-            href="https://linkedin.com/in/kpethodaniel"
-            className="cursor"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Linkedin
-          </a>
-          <a
-            href="https://github.com/danielpetho"
-            className="cursor"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>
+      <div className="flex flex-col md:flex-row max-w-[1920px] px-12 sm:px-16 md:px-20 lg:px-32 bg-black pb-24 sm:pb-20 md:pb-16 lg:pb-12 pt-12 w-full justify-start md:justify-between">
+        <div className="flex flex-col xl:flex-row w-full">
+          <MaskedExternalLink text={"Instagram"} link="https://instagram.com/nonzeroexitcode" tailwindClasses="" delay={0.5} />
+          <MaskedExternalLink text={"Twitter / X"} link="https://twitter.com/nonzeroexitcode" tailwindClasses="" delay={0.6} />
+          <MaskedExternalLink text={"Linkedin"} link="https://linkedin.com/in/kpethodaniel" tailwindClasses="" delay={0.7} />
+          <MaskedExternalLink text={"Github"} link="https://github.com/danielpetho" tailwindClasses="" delay={0.8} />
         </div>
 
         <div className="flex flex-row mt-12 md:mt-0">
--- app/components/Text/MaskedExternalLink.tsx
+++ app/components/Text/MaskedExternalLink.tsx
@@ -2,15 +2,32 @@
 
 import { motion, useInView } from "framer-motion";
 import { useRef } from "react";
+import { IoMdArrowForward } from "react-icons/io";
 
 interface MaskedExternalLinkProps {
-  phrases: string[];
+  text: string;
+  link: string;
   tailwindClasses: string;
   delay: number;
 }
 
+const arrowIconVariants = {
+  hover: {
+    rotate: -45,
+    transition: { duration: 0.33, ease: [0.76, 0, 0.24, 1] },
+  },
+};
+
+const headerVariants = {
+  hover: {
+    x: 40,
+    transition: { duration: 0.33, ease: [0.76, 0, 0.24, 1] },
+  },
+};
+
 const MaskedExternalLink: React.FC<MaskedExternalLinkProps> = ({
-  phrases,
+  text,
+  link,
   tailwindClasses,
   delay,
 }) => {
@@ -18,24 +35,29 @@ const MaskedExternalLink: React.FC<MaskedExternalLinkProps> = ({
   const isInView = useInView(body, { once: true, margin: "75%" });
 
   return (
-    <div ref={body} className={`${tailwindClasses}`}>
-      {phrases &&
-        phrases.length > 0 &&
-        phrases.map((phrase: string, idx: number) => {
-          return (
-            <div key={idx} className="overflow-hidden">
-              <motion.h1
-                initial={{ opacity: 0, y: 50, filter: "blur(5px)" }}
-                transition={{ duration: 0.5, ease: "easeInOut", delay: delay }}
-                whileInView={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-                viewport={{ once: true }}
-              >
-                {phrase}
-              </motion.h1>
-            </div>
-          );
-        })}
-    </div>
+    <motion.div
+      ref={body}
+      whileHover="hover"
+      className={`${tailwindClasses} w-full flex flex-row flex-grow overflow-hidden`}
+    >
+      <motion.div className="relative" variants={headerVariants}>
+        <motion.span className="absolute left-0 -ml-8 mt-0.5 text-2xl " variants={arrowIconVariants}>
+            <IoMdArrowForward />
+        </motion.span>
+
+        <motion.a
+          initial={{ opacity: 0, y: 50, filter: "blur(5px)" }}
+          transition={{ duration: 0.5, ease: "easeInOut", delay: delay }}
+          whileInView={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+          viewport={{ once: true }}
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {text}
+        </motion.a>
+      </motion.div>
+    </motion.div>
   );
 };
 
