0aa80f5a99aac8e8ae06051ad1aebd4d2fc197be
danielpetho <kpethodaniel@gmail.com>
2023-12-16 22:00:00 +0100

    mobile view filter

--- app/lab/filters/[slug]/page.tsx
+++ app/lab/filters/[slug]/page.tsx
@@ -19,13 +19,13 @@ async function Filter({ params: { slug } }: PageProps) {
   return (
     <div className="sm:h-screen w-screen flex flex-col sm:my-12 my-24 justify-center items-center">
       <div className="sm:max-w-7xl flex flex-col text-left items-start justify-center">
-        <div className="text-slate-500">
+        <div className="text-slate-500 mx-6 sm:mx-0 md:mx-0">
           <IoMdArrowForward className="inline-block mr-2 rotate-180 mb-0.5" />
           <Link href="/lab/filters">All filters</Link>
         </div>
-        <div className="flex justify-center items-center w-full md:w-[80vw] lg:w-[60vw] xl:w-[50vw] 2xl:w-[35vw] 3xl:w-[30vw]">
+        <div className="flex justify-center items-center w-full sm:w-[80vw] lg:w-[60vw] xl:w-[50vw] 2xl:w-[35vw] 3xl:w-[30vw]">
           {filter ? (
-            <div className="flex sm:flex-row flex-col justify-center w-full items-center  md:bg-pale-white md:shadow-xl md:rounded-[50px] p-6 sm:p-14">
+            <div className="flex sm:flex-row flex-col justify-center w-full items-center  sm:bg-pale-white sm:shadow-xl sm:rounded-[50px] p-6 sm:p-14">
               <div className="rounded-[25px] sm:rounded-[30px] relative overflow-hidden w-full sm:w-2/5 ">
                 <video
                   src={filter.preview.url}
@@ -37,23 +37,23 @@ async function Filter({ params: { slug } }: PageProps) {
                 ></video>
               </div>
 
-              <div className="flex flex-col sm:w-3/5 w-full sm:pl-12 h-full">
+              <div className="flex flex-col sm:w-3/5 w-full mt-6 sm:mt-0 sm:pl-12 h-full">
                 <h1 className="text-3xl md:text-4xl font-semibold">
                   {filter.name}
                 </h1>
-                <ul className="flex flex-row pt-4 gap-2">
+                <ul className="flex-row pt-4 gap-3 hidden sm:flex">
                   {filter.snapchatLink && (
-                    <p className="border border-black rounded-full px-3 py-1.5">
+                    <p className="border border-black rounded-full px-4 py-1.5">
                       Snapchat
                     </p>
                   )}
                   {filter.instagramLink && (
-                    <p className="border border-black rounded-full px-3 py-1.5">
+                    <p className="border border-black rounded-full px-4 py-1.5">
                       Instagram
                     </p>
                   )}
                   {filter.tiktokLink && (
-                    <p className="border border-black rounded-full px-3 py-1.5">
+                    <p className="border border-black rounded-full px-4 py-1.5">
                       TikTok
                     </p>
                   )}
