6b345406b7147e0d86b17bfc4bca024f272d1166
danielpetho <kpethodaniel@gmail.com>
2024-01-07 12:04:49 +0100

    remove lamina

--- app/components/R3F/Scene.tsx
+++ app/components/R3F/Scene.tsx
@@ -3,21 +3,15 @@
 import { Canvas } from '@react-three/fiber'
 import { Preload } from '@react-three/drei'
 import { r3f } from '@/src/utils/r3f/tunnel-rat'
-import { EffectComposer } from '@react-three/postprocessing'
-import Grain from './PostPro/Grain'
-import { useRef } from 'react'
 import { Perf } from 'r3f-perf'
 
 export default function Scene({ ...props }) {
-  const grainEffectRef = useRef();
 
   return (
     <Canvas {...props} shadows>
       {/* @ts-ignore */}
       <r3f.Out />
 
-      <Preload all />
-      <Perf openByDefault trackGPU={true} position={'bottom-right'} />
     </Canvas>
   )
 }
